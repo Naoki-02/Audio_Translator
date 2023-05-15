232daf7ee9f3dae2bfd7d2e031717b23d39b4693
Naoki-02 <smile.naonao@icloud.com>
2023-05-15 13:50:16 +0900

    pathからファイル名取得

--- audio_translator.py
+++ audio_translator.py
@@ -1,12 +1,13 @@
 import whisper
 import codecs
+import os
 model=whisper.load_model("tiny")
 
 #result = model.transcribe("ファイル名")
 path="test.mp3"
 result = model.transcribe(path,fp16=False)
 
-with codecs.open('contents/test_w.txt',mode='w',encoding='utf-8') as f:
+with codecs.open('contents/'+os.path.splitext(os.path.basename(path))[0]+'.txt',mode='w',encoding='utf-8') as f:
     f.write(result["text"])
     
 print(result["text"])
