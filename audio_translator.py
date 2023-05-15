d12b46c89d702fb9e79e774c176259869d0003f0
Naoki-02 <smile.naonao@icloud.com>
2023-05-15 13:27:35 +0900

    txt出力機能実装

--- audio_translator.py
+++ audio_translator.py
@@ -1,9 +1,12 @@
 import whisper
-
-model=whisper.load_model("base")
+import codecs
+model=whisper.load_model("tiny")
 
 #result = model.transcribe("ファイル名")
+path="test.mp3"
+result = model.transcribe(path,fp16=False)
 
-result = model.transcribe("test.mp3",fp16=False)
-
+with codecs.open('contents/test_w.txt',mode='w',encoding='utf-8') as f:
+    f.write(result["text"])
+    
 print(result["text"])
