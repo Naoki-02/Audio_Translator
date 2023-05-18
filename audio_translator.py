2d8d6f745c332b33b9d77362ada7d352ef394481
Naoki-02 <smile.naonao@icloud.com>
2023-05-18 14:29:21 +0900

    翻訳機能&file例外

--- audio_translator.py
+++ audio_translator.py
@@ -8,21 +8,26 @@ model=whisper.load_model("tiny")
 #result = model.transcribe("ファイル名")
 path="english.mp3"
 
-result = model.transcribe(path,fp16=False)
-save=input()
 
-#保存するかしないかの処理
-if save == '1':
-    #テキストファイルに出力し保存する
-    with codecs.open('contents/'+os.path.splitext(os.path.basename(path))[0]+'.txt',mode='w',encoding='utf-8') as f:
-        f.write(result["text"])
-        print("保存しました。")
+r=os.path.exists(path)
+if(r==True):
+    result = model.transcribe(path,fp16=False)
+    save=input()
 
-        
-#日本語以外の場合翻訳して表示する
-if result['language'] != 'ja':        
-    translated=GoogleTranslator(source='auto',target='ja').translate(result['text'])
-    print(translated)
+    #保存するかしないかの処理
+    if save == '1':
+        #テキストファイルに出力し保存する
+        with codecs.open('contents/'+os.path.splitext(os.path.basename(path))[0]+'.txt',mode='w',encoding='utf-8') as f:
+            f.write(result["text"])
+            print("保存しました。")
+                        
+    #日本語以外の場合翻訳して表示する
+    if result['language'] != 'ja':        
+        translated=GoogleTranslator(source='auto',target='ja').translate(result['text'])
+        print(translated)
 
-print(result["text"])
+    print(result["text"])
+else:
+    print("ファイルが見つかりません")
+
 
